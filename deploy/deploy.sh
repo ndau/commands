5125929261e45ac79ce7d4ad356fc3f55fbd8a77
Josh <fetmar@gmail.com>
2018-12-11 12:53:12 -0500

    remove old TODOs, fix remaining magic string, move coreutils to circle-ci image and bump version,

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -13,6 +13,8 @@ if [ "${CIRCLE_BRANCH}" == "$ECR_PUSH_BRANCH" ]; then
   cd /root/automation/testnet
 
   # create new multinode test net
+  # Use $SHA here instead of the automation's git ls-remote method.
+
   CHAOSNODE_TAG=$SHA \
   NDAUNODE_TAG=$SHA \
   RELEASE=$RELEASE_NAME \
