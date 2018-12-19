7bbc75a0ee54e6d8cd5ea78a5d39112b3b6f0d76
Eric Schmidt <eric.schmidt@oneiro.io>
2018-12-19 15:22:47 -0800

    Add generate.sh

---
+++ bin/generate.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Wrapper for `go generate` for the repo specified.  Usage: ./generate.sh <repo>
+# Must have msgp installed.  See https://github.com/oneiro-ndev/chaos/README.md for details.
+
+# Load our environment variables.
+CMDBIN_DIR="$(go env GOPATH)/src/github.com/oneiro-ndev/commands/bin"
+# shellcheck disable=SC1090
+source "$CMDBIN_DIR"/env.sh
+
+REPO="$1"
+if [ -z "$REPO" ]; then
+    echo Usage examples:
+    echo "  ./generate.sh metanode"
+    echo "  ./generate.sh chaos"
+    echo "  ./generate.sh ndau"
+    exit 1
+fi
+
+cd "$NDEV_DIR/$REPO"
+go generate ./...
+
+# Strip out all the "msgp.WrapError" lines that don't compile in unit tests.
+GEN=$(git diff-index HEAD --name-only)
+for f in $GEN;
+do
+    if [[ "$f" == *_gen.go ]]; then
+        grep -v "^.*msgp.WrapError.*$" "$f" > "$f-tmp" && mv "$f-tmp" "$f"
+    fi
+done
