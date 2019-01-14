30c072f6e34461c16922e395edcba32a3d5731aa
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2019-01-14 15:20:39 +0100

    upgrade tendermint versions

--- bin/conf.sh
+++ bin/conf.sh
@@ -20,6 +20,8 @@ done
 echo Configuring tendermint...
 cd "$TENDERMINT_DIR" || exit 1
 
+go build ./cmd/tendermint
+
 for node_num in $(seq 0 "$HIGH_NODE_NUM");
 do
     tm_chaos_home="$TENDERMINT_CHAOS_DATA_DIR-$node_num"
