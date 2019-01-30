dde7fce8d8deae36d4cf9e62fb98d7405c984696
Eric Schmidt <eric.schmidt@oneiro.io>
2019-01-30 10:17:34 -0800

    Add node num param to nodeurl.sh

--- bin/nodeurl.sh
+++ bin/nodeurl.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
-NET=$1   # devnet or testnet
-CHAIN=$2 # ndau or chaos
+NODE_NET=$1 # devnet or testnet
+NODE_NUM=$2 # 0-based node number
+CHAIN=$3    # ndau or chaos
 
-if [ -z "$NET" ]; then
+if [ -z "$CHAIN" ]; then
     echo nodeurl: Echo the URL for a tendermint node.
     echo Must have kubectl set up.  See integration-tests repo README.md for details.
     echo Usage:
-    echo "  ./nodeurl.sh NET CHAIN"
+    echo "  ./nodeurl.sh NODE_NET NODE_NUM CHAIN"
     echo Example:
-    echo "  ./nodeurl.sh devnet ndau"
+    echo "  ./nodeurl.sh devnet 0 ndau"
     exit 1
 fi
 
 # These commands were adapted from integration-tests/conftest.py:
 ADDRESS=$(kubectl get nodes -o jsonpath='{.items[*].status.addresses[?(@.type=="ExternalIP")].address}' | tr " " "\n" | head -n 1 | tr -d "[:space:]")
-PORT=$(kubectl get service --namespace default -o jsonpath='{.spec.ports[?(@.name=="rpc")].nodePort}' "$NET-0-nodegroup-$CHAIN-tendermint-service")
+PORT=$(kubectl get service --namespace default -o jsonpath='{.spec.ports[?(@.name=="rpc")].nodePort}' "$NODE_NET-$NODE_NUM-nodegroup-$CHAIN-tendermint-service")
 
 # User can use this URL in curl commands.
 echo "http://$ADDRESS:$PORT"
