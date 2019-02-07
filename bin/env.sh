d76d45c6f08f2aae3ab79a751c875e64f67b4445
Eric Schmidt <eric.schmidt@oneiro.io>
2019-02-07 11:07:43 -0800

    Use localnet for honeycomb dataset on localnet

--- bin/env.sh
+++ bin/env.sh
@@ -31,6 +31,7 @@ export TM_RPC_PORT=26670
 BPC_OPS_ACCT_NAME=bpc-operations
 # This is the chaos-side identity that will be associated with the BPC account.
 SYSVAR_ID=sysvar
+HONEYCOMB_DATASET=localnet
 
 # Go source path.
 GO_DIR=$(go env GOPATH)
