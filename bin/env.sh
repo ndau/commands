155be0a46e1f2420e6f7a76f77bca1f2413947b3
Eric Schmidt <eric.schmidt@oneiro.io>
2019-02-11 08:16:36 -0800

    Add comment about honeycomb dataset

--- bin/env.sh
+++ bin/env.sh
@@ -32,6 +32,7 @@ BPC_OPS_ACCT_NAME=bpc-operations
 # This is the chaos-side identity that will be associated with the BPC account.
 SYSVAR_ID=sysvar
 
+# Set a honeycomb dataset for localnet if one isn't currently present in an environment variable.
 if [[ -z "$HONEYCOMB_DATASET" ]]; then
     export HONEYCOMB_DATASET=localnet
 fi
