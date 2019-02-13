75a8f34c56eaa55766ffeb68727d4718cfc7985e
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2019-02-13 11:21:48 +0100

    display info field to stderr

--- cmd/ndau/delegates.go
+++ cmd/ndau/delegates.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	cli "github.com/jawher/mow.cli"
 	"github.com/oneiro-ndev/ndau/pkg/tool"
@@ -13,6 +14,10 @@ func getDelegates(verbose *bool) func(*cli.Cmd) {
 			conf := getConfig()
 			delegates, resp, err := tool.GetDelegates(tmnode(conf.Node, nil, nil))
 
+			// errors get logged in the Info field
+			fmt.Fprint(os.Stderr, resp.Response.GetInfo())
+
+			// display the delegates
 			for node, delegated := range delegates {
 				fmt.Printf("%s:\n", node)
 				for _, d := range delegated {
