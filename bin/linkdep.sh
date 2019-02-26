2807e5446f599bf0a48472c0a59af437843555e1
Kent Quirk <kentquirk@gmail.com>
2019-02-26 15:32:33 -0500

    Linkdep with no args now useful
    In addition to a usage command, it also lists the currently-linkdep'd repositories.

--- bin/linkdep.sh
+++ bin/linkdep.sh
@@ -37,6 +37,11 @@ if [ -z "$REPO" ]; then
     echo linkdep: Link or unlink vendor directories of a repo, for build or test purposes
     echo Usage:
     echo "  ./linkdep.sh {metanode|chaos|ndau|genesis|all} [-l|--link] [-u|--unlink] [-b|--build] [-t|--test]"
+    current="$(find vendor/github.com/oneiro-ndev -type l -depth 1 |sed 's:vendor/github.com/oneiro-ndev/:  :')"
+    if [[ -n "$current" ]]; then
+        echo Currently linked:
+        echo "$current"
+    fi
     exit 1
 fi
 
