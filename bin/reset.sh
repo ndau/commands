9c2f52f8edfccea3bbf4d9689172793dbe70f388
Eric Schmidt <eric.schmidt@oneiro.io>
2019-03-05 11:37:26 -0800

    Add arg to reset.sh for changing node count

--- bin/reset.sh
+++ bin/reset.sh
@@ -4,6 +4,24 @@ CMDBIN_DIR="$(go env GOPATH)/src/github.com/oneiro-ndev/commands/bin"
 # shellcheck disable=SC1090
 source "$CMDBIN_DIR"/env.sh
 
+# Optionally change the number of nodes in the localnet setup.
+node_count="$1"
+if [[ ! -z "$node_count" ]]; then
+    if [[ ! "$node_count" =~ ^[0-9]+$ ]]; then
+        echo Node count must be a positive integer
+        exit 1
+    fi
+    if [ "$node_count" -lt 1 ] || [ "$node_count" -gt "$MAX_NODE_COUNT" ]; then
+        echo Node count must be in [1, "$MAX_NODE_COUNT"]
+        exit 1
+    fi
+
+    echo "$node_count" > "$NODE_COUNT_FILE"
+
+    export NODE_COUNT="$node_count"
+    export HIGH_NODE_NUM=$((NODE_COUNT - 1))
+fi
+
 # Kill everything before we wipe the pid files.
 "$CMDBIN_DIR"/kill.sh
 
