ef41f1c8e6023b0d016425fab9c1867310016fd3
Josh <fetmar@gmail.com>
2019-03-05 15:58:00 -0500

    add redis image, add utilities to noms, redis, tendermint images, use deps-cache to speedup builds, update versions, use labels for image tags, switch noms to oneiro-ndev repo,

--- deploy/tendermint/docker-build-push.sh
+++ deploy/tendermint/docker-build-push.sh
@@ -2,7 +2,12 @@
 
 echo "Starting $0"
 
+# get the directory of this file
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 # grab TM version from Docker file
+CONTAINER_VERSION=$(grep "org.opencontainers.image.version" "$DIR"/tendermint.docker | sed "s/.* \([v0-9].*\)/\1/")
+
 TM_CONTAINER_VERSION=$(grep -e 'TM_VERSION_TAG' /commands/deploy/tendermint/tendermint.docker -m 1 | cut -f3 -d ' ')
 version_check=$(aws ecr describe-images --repository-name tendermint | jq ".imageDetails[].imageTags[]? | select (. == \"${TM_CONTAINER_VERSION}\")")
 
