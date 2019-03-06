e478f29ce9783cb831e5126e7761b94d72dc407b
Josh <fetmar@gmail.com>
2019-03-06 15:46:35 -0500

    remove branch specification and add check for deps-cache upload

--- deploy/deps-cache/update-deps-cache.sh
+++ deploy/deps-cache/update-deps-cache.sh
@@ -13,6 +13,12 @@ fi
 # get the version label from the docker file
 CONTAINER_VERSION=$(grep "org.opencontainers.image.version.*" "$DIR"/deps-cache.docker | sed "s/.* \([v0-9].*\)/\1/")
 
+version_check=$(aws ecr describe-images --repository-name deps-cache | jq ".imageDetails[].imageTags[]? | select (. == \"$CONTAINER_VERSION\")")
+# only push if we have a different version.
+if [ ! -z "$version_check" ]; then
+  echo "deps-cache container version $CONTAINER_VERSION already exists. Will not push." >&2
+  exit 0
+fi
 # make a version tag
 TAG=578681496768.dkr.ecr.us-east-1.amazonaws.com/deps-cache:$CONTAINER_VERSION
 
