f56e1efc63232ca394ba34a5033bda66963f3121
Eric Schmidt <eric.schmidt@oneiro.io>
2019-03-28 10:11:42 -0700

    Make switching snapshot to use in demo scripts easier

--- docker/demo/run2.sh
+++ docker/demo/run2.sh
@@ -5,8 +5,10 @@ cd "$SCRIPT_DIR" || exit 1
 
 IP=$(./get_ip.sh)
 
+SNAPSHOT=$(./get_snapshot.sh)
+
 ../bin/runcontainer.sh \
     localnet-2 26662 26672 3032 \
     "$IP:26660:26670,$IP:26661:26671" \
-    snapshot-localnet-2 \
+    $SNAPSHOT \
     ../../bin/ndau-snapshots/node-identity-2.tgz
--- docker/demo/run3.sh
+++ docker/demo/run3.sh
@@ -5,8 +5,10 @@ cd "$SCRIPT_DIR" || exit 1
 
 IP=$(./get_ip.sh)
 
+SNAPSHOT=$(./get_snapshot.sh)
+
 ../bin/runcontainer.sh \
     localnet-3 26663 26673 3033 \
     "$IP:26660:26670,$IP:26661:26671,$IP:26662:26672" \
-    snapshot-localnet-2 \
+    $SNAPSHOT \
     ../../bin/ndau-snapshots/node-identity-3.tgz
