9c2c3408eed71211fa74031209680ab0c021b8d3
Ed McNierney <ed@mcnierney.com>
2019-04-10 14:59:16 -0400

    Allow ETL to respect net setup choice
    If ETL is run, the source data is now read from a directory specific to the net (localnet, testnet, mainnet) being set up. ETL data must live in `genesis/localnet-genesis`, `genesis/testnet-genesis`, or `genesis/mainnet-genesis` as appropriate.

--- bin/etl.sh
+++ bin/etl.sh
@@ -3,7 +3,7 @@
 echo ETL for ndau noms
 # cd to genesis dir because etl command looks for ./config.toml there
 echo setting CWD to $GO_DIR/src/github.com/oneiro-ndev/genesis
-pushd $NDEV_DIR/genesis/mainnet-genesis/etl || exit 1
+pushd $NDEV_DIR/genesis/$CHAIN_ID-genesis/etl || exit 1
 # if spreadsheet file doesn't exist, die
 # if [ ! -e "$SPREADSHEET_FILE" ]; then
 #    echo spreadsheet file: \"$SPREADSHEET_FILE\" does not exist, set variable \$SPREADSHEET_FILE to spreadsheet file name, exiting
