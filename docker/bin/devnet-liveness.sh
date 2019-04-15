8bf136783d449b1b3e4954aa9b4269f74fcaf27b
Josh <fetmar@gmail.com>
2019-04-15 13:28:05 -0400

    update persistent peer ids, fix docker caching, add node network variables for integration tests

--- docker/bin/devnet-liveness.sh
+++ docker/bin/devnet-liveness.sh
@@ -2,12 +2,12 @@
 
 errcho() { >&2 echo -e "$@"; }
 
-# test the liveness of testnet
+# test the liveness of devnet
 errcho "\nTesting tendermint RPC"
-curl https://api.ndau.tech:3010{0..4}/status -w "\n"
+curl -s https://api.ndau.tech:3010{0..4}/status | jq ".result.node_info.id"
 
 errcho "\nTesting ndauapi"
-curl https://api.ndau.tech:3030{0..4}/node/health -w "\n"
+curl -s https://api.ndau.tech:3030{0..4}/node/health -w "\n" | jq -c
 
 errcho "\nTesting Tendermint p2p"
 nc -z 50.17.109.111 30200-30204
--- docker/bin/testnet-liveness.sh
+++ docker/bin/testnet-liveness.sh
@@ -4,10 +4,10 @@ errcho() { >&2 echo -e "$@"; }
 
 # test the liveness of testnet
 errcho "\nTesting tendermint RPC"
-curl https://api.ndau.tech:3110{0..4}/status -w "\n"
+curl -s https://api.ndau.tech:3110{0..4}/status | jq ".result.node_info.id"
 
 errcho "\nTesting ndauapi"
-curl https://api.ndau.tech:3130{0..4}/node/health -w "\n"
+curl -s https://api.ndau.tech:3130{0..4}/node/health -w "\n" | jq -c
 
 errcho "\nTesting Tendermint p2p"
 nc -z 50.17.109.111 31200-31204
