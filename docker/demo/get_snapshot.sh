72bdbd9b84c9199f7fc6d082da6fd914a79165e4
Eric Schmidt <eric.schmidt@oneiro.io>
2019-04-26 10:42:17 -0700

    Add comment to get_snapshot.sh

--- docker/demo/get_snapshot.sh
+++ docker/demo/get_snapshot.sh
@@ -2,4 +2,5 @@
 
 # Useful for all the demo run scripts to use the same snapshot.
 # See the docker/README.md for instructions on using a demo snapshot and node-identity files.
+# Using "" for this will cause runcontainer.sh to pull down the latest snapshot for $NDAU_NETWORK.
 echo snapshot-localnet-demo
