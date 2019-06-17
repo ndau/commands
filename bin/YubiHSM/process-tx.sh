dfdbac82f3a7b9e2bc6dc476754808ec4340aa59
Ed McNierney <ed@mcnierney.com>
2019-06-17 13:51:49 -0400

    Fix `process-tx.sh` for multiple object IDs

--- bin/YubiHSM/process-tx.sh
+++ bin/YubiHSM/process-tx.sh
@@ -1,24 +1,31 @@
 #! /bin/bash
 
-# Usage: process-tx.sh <address> <txtype> <json> <YubiHSM object ID> <'main' or 'test'>
+# Usage: process-tx.sh <address> <txtype> <json file> <'main' or 'test'> <number of signatures>
+
 NDAUTOOL="$GOPATH/src/github.com/oneiro-ndev/commands/ndau"
 KEYTOOL="$GOPATH/src/github.com/oneiro-ndev/commands/keytool"
 SIGN="$GOPATH/src/github.com/oneiro-ndev/commands/bin/YubiHSM/sign.py"
 YUBIHSM="/usr/local/bin/yubihsm-shell"
 
-NODE='https://'$5'net-2.ndau.tech:3030'
+NODE='https://'$4'net-2.ndau.tech:3030'
 
-export SEQ=$(( `curl --silent --get $NODE/account/account/$1 | jq '.[].sequence'` + 1 ))
-TX=`cat $3 | jq '.sequence = (env.SEQ | tonumber)'`
-SB=`$NDAUTOOL signable-bytes $2 <<< $TX`
+SEQ=$(( `curl --silent --get $NODE/account/account/$1 | jq ".$1.sequence"` + 1))
+TX=`cat $3 | jq ".sequence = $SEQ"`
+echo $TX
 
-read -p "Insert YubiHSM key and press [Enter] to continue"
+SB=`echo $TX | $NDAUTOOL signable-bytes $2`
 
-SIG=`$YUBIHSM --connector=yhusb:// --action sign-eddsa -authkey 101 --object-id $4 --algorithm ed25519 --informat base64 --outformat base64 | $KEYTOOL ed raw signature --stdin -b`
-TXSIGNED=`cat $3 | jq '.signatures += ["$SIG"]'`
+for i in $(seq 1 1 $5);
+do
+    read -p "Insert YubiHSM key $i - enter object ID for signing: " OBJID
+    SIG=`echo $SB | $YUBIHSM --connector=yhusb:// --action sign-eddsa --authkey 101 --object-id $OBJID --algorithm ed25519 --informat base64 --outformat base64 | $KEYTOOL ed raw signature --stdin -b`
+    TX=`echo $TX | jq ".signatures += [\"$SIG\"]"`
+done
 
-read -p "\nPress [Enter] to prevalidate"
-curl -s -d "$TXSIGNED" -H "Content-Type: application/json" -X POST $NODE/tx/prevalidate/rfe | jq .
+echo $TX | jq .
 
-read -p "\nPress [Enter] to submit"
-curl -s -d "$TXSIGNED" -H "Content-Type: application/json" -X POST $NODE/tx/submit/rfe | jq .
+read -p "Press [Enter] to prevalidate"
+curl -s -d "$TX" -H "Content-Type: application/json" -X POST $NODE/tx/prevalidate/$2 | jq .
+
+read -p "Press [Enter] to submit"
+curl -s -d "$TX" -H "Content-Type: application/json" -X POST $NODE/tx/submit/$2 | jq .
