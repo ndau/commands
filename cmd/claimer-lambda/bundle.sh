99bfdede18a470e0c5a02f1164fb59d9cbf09ee5
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2019-07-22 14:00:40 +0200

    claimer-lambda bundle script updates aws function

--- cmd/claimer-lambda/bundle.sh
+++ cmd/claimer-lambda/bundle.sh
@@ -4,4 +4,10 @@ set -e
 
 cd "$(go env GOPATH)/src/github.com/oneiro-ndev/commands"
 GOOS=linux go build ./cmd/claimer-lambda
-zip cmd/claimer-lambda/claimer-lambda.zip claimer-lambda
+zf="cmd/claimer-lambda/claimer-lambda.zip"
+zip "$zf" claimer-lambda
+rm claimer-lambda # get rid of linux binary
+aws lambda update-function-code \
+    --function-name "claimer-service" \
+    --zip-file "fileb://$zf" \
+    --publish
