0e8da6c6e095d1426188d1bfab53b2268cca3b19
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2019-07-29 18:00:47 +0200

    bundler: differentiate testnet and mainnet
    also make the output much more human-friendly

--- cmd/claimer-lambda/bundle.sh
+++ cmd/claimer-lambda/bundle.sh
@@ -2,12 +2,71 @@
 
 set -e
 
+if [ -n "$(git status --porcelain)" ]; then
+    echo "branch is dirty; commit and try again"
+    exit 1
+fi
+hash=$(git rev-parse --short HEAD)
+
+echo "compiling..."
 cd "$(go env GOPATH)/src/github.com/oneiro-ndev/commands"
 GOOS=linux go build ./cmd/claimer-lambda
+echo "zipping..."
 zf="cmd/claimer-lambda/claimer-lambda.zip"
-zip "$zf" claimer-lambda
+zip -9 "$zf" claimer-lambda
+du -h "$zf"
 rm claimer-lambda # get rid of linux binary
-aws lambda update-function-code \
-    --function-name "claimer-service" \
-    --zip-file "fileb://$zf" \
-    --publish
+
+echo "uploading..."
+# publish the latest code
+data=$(
+    aws lambda update-function-code \
+        --function-name "claimer-service" \
+        --zip-file "fileb://$zf"
+)
+code_sha=$(echo "$data" | jq -r .CodeSha256)
+revision=$(echo "$data" | jq -r .RevisionId)
+
+update_net() {
+    net=$1
+
+    echo "updating $net..."
+
+    echo "  updating environment and description..."
+    data=$(
+        aws lambda update-function-configuration \
+            --function-name "claimer-service" \
+            --description "claim service revision=$hash net=$net" \
+            --environment "{
+                    \"Variables\": {
+                        \"PORT\": \"80\",
+                        \"S3_CONFIG_BUCKET\": \"claimer-service-config\",
+                        \"S3_CONFIG_PATH\": \"claimer-conf-${net}.toml\"
+                    }
+                }"
+    )
+    revision=$(echo "$data" | jq -r .RevisionId)
+
+    echo "  publishing version..."
+    data=$(
+        aws lambda publish-version \
+            --function-name "claimer-service" \
+            --revision-id "$revision" \
+            --code-sha256 "$code_sha"
+    )
+    version=$(echo "$data" | jq -r .Version)
+
+    echo "  updating alias..."
+    data=$(
+        aws lambda update-alias \
+            --function-name "claimer-service" \
+            --name "$net" \
+            --function-version "$version"
+    )
+    revision=$(echo "$data" | jq -r .RevisionId)
+
+    echo "  done!"
+}
+
+update_net testnet
+update_net mainnet
