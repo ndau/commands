b7a42d847bd3810ac53368920fd6412371a46376
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2019-08-05 15:45:41 +0200

    add a touch more functionality to cleanup script

--- docker/bin/cleanupdocker.sh
+++ docker/bin/cleanupdocker.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-# This frees up space from old and unused docker images.
+# clean up dead containers
 dead_containers=$(docker ps -q -f 'status=exited')
 if [ -n "$dead_containers" ]; then
     docker rm "$dead_containers"
 fi
-yes | docker image prune
+# clean up dangling images
+docker image prune -f
+# clean up old version of ndauimage
+docker images ndauimage --filter=before=ndauimage:latest --format="{{.ID}}" |\
+    xargs docker image rm
