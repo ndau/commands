6691a324395c4151dbdcf484016c459ef7a9a835
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2019-08-05 15:07:22 +0200

    update ancillary docker scripts

--- docker/bin/cleanupdocker.sh
+++ docker/bin/cleanupdocker.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 # This frees up space from old and unused docker images.
-docker rm $(docker ps -q -f 'status=exited')
-docker rmi $(docker images -q -f "dangling=true")
+dead_containers=$(docker ps -q -f 'status=exited')
+if [ -n "$dead_containers" ]; then
+    docker rm "$dead_containers"
+fi
+yes | docker image prune
