8c6df5eb5cbf9ae42925822a4841685853725db6
Eric Schmidt <eric.schmidt@oneiro.io>
2019-08-15 16:33:28 -0700

    Use streamlined circle-ci image

--- deploy/integration.sh
+++ deploy/integration.sh
@@ -2,11 +2,21 @@
 
 # This is the ENTRYPOINT for the tests-container in the integration Circle CI job.
 
+set -e
+
 export NDAUHOME=/.ndau
 
 echo "Configuring ndau tool..."
-./ndau conf http://$IP:26670
-./ndau conf update-from /system_accounts.toml
+/image/bin/ndau conf http://$IP:26670
+/image/bin/ndau conf update-from /system_accounts.toml
+
+echo "Setting up python environment..."
+python3 -m ensurepip
+rm -r /usr/lib/python*/ensurepip
+pip3 install --upgrade pip setuptools
+pip3 install pytest pipenv
+if [ ! -e /usr/bin/pip ]; then ln -s pip3 /usr/bin/pip; fi
+if [ ! -e /usr/bin/python ]; then ln -sf /usr/bin/python3 /usr/bin/python; fi
 
 echo "Running integration tests..."
 cd /integration-tests
