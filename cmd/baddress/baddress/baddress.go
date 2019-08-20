edf6d468720f157c5873e744548d211b4c2e4f26
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2019-08-20 13:55:14 +0200

    implement setting bad addresses

--- cmd/baddress/baddress/baddress.go
+++ cmd/baddress/baddress/baddress.go
@@ -3,25 +3,74 @@ package baddress
 import (
 	"fmt"
 
+	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
 	"github.com/oneiro-ndev/ndaumath/pkg/address"
+	"github.com/pkg/errors"
+)
+
+// Table is the table name for the bad address dynamodb table
+const Table = "bad-addresses"
+
+// Region is the AWS region to use
+const Region = "us-east-1"
+
+const (
+	addressField = "address"
+	pathField    = "derivation-path"
 )
 
 // A BadAddress is one users should not use
 type BadAddress struct {
-	Address address.Address `arg:"positional,required" help:"an address users should never use"`
-	Path    string          `arg:"-p" help:"derivation path for the bad address"`
-	Reason  string          `arg:"-r" help:"why the user should not use this address"`
+	Address address.Address `json:"address" arg:"positional,required" help:"an address users should never use"`
+	Path    string          `json:"derivation-path" arg:"-p" help:"derivation path for the bad address"`
+	Reason  string          `json:"reason" arg:"-r" help:"why the user should not use this address"`
+}
+
+func (b BadAddress) marshal() (map[string]*dynamodb.AttributeValue, error) {
+	// ddb attrs can't be empty strings
+	if b.Path == "" {
+		b.Path = "/"
+	}
+
+	m, err := dynamodbattribute.MarshalMap(b)
+	if err != nil {
+		return nil, errors.Wrap(err, "initial aws marshaling")
+	}
+	// we need to override the default marshaling for the address;
+	// ddbattr.MarshalMap isn't quite smart enough for our crazy type patterns
+	m[addressField], err = dynamodbattribute.Marshal(b.Address.String())
+	if err != nil {
+		return nil, errors.Wrap(err, "override address marshaling")
+	}
+	return m, nil
 }
 
 // Add a bad address to the DB
-func Add(ddb *dynamodb.DynamoDB, addr BadAddress) error {
-	fmt.Println("add: unimplemented")
+func Add(ddb *dynamodb.DynamoDB, addr BadAddress, verbose bool) error {
+	if verbose {
+		fmt.Printf("unmarshaled form:\n%#v\n", addr)
+	}
+	item, err := addr.marshal()
+	if err != nil {
+		return errors.Wrap(err, "marshaling bad address")
+	}
+	if verbose {
+		fmt.Printf("marshaled form:\n%#v\n", item)
+	}
+	_, err = ddb.PutItem(&dynamodb.PutItemInput{
+		TableName: aws.String(Table),
+		Item:      item,
+	})
+	if err != nil {
+		return errors.Wrap(err, "put address in db")
+	}
 	return nil
 }
 
 // Remove a bad address from teh DB
-func Remove(ddb *dynamodb.DynamoDB, addr BadAddress) error {
+func Remove(ddb *dynamodb.DynamoDB, addr BadAddress, verbose bool) error {
 	fmt.Println("remove: unimplemented")
 	return nil
 }
--- cmd/baddress/main.go
+++ cmd/baddress/main.go
@@ -5,19 +5,28 @@ import (
 	"os"
 
 	"github.com/alexflint/go-arg"
+	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
 	"github.com/oneiro-ndev/commands/cmd/baddress/baddress"
+	"github.com/oneiro-ndev/ndaumath/pkg/address"
 )
 
 // GenerateCmd handles args for generation
 type GenerateCmd struct {
 }
 
+// CheckCmd handles args for checking
+type CheckCmd struct {
+	Address address.Address `arg:"positional,required" help:"check this address"`
+}
+
 var args struct {
 	Generate *GenerateCmd         `arg:"subcommand:generate" help:"automatically generate bad addresses"`
 	Add      *baddress.BadAddress `arg:"subcommand:add" help:"manually add bad addresses"`
 	Remove   *baddress.BadAddress `arg:"subcommand:remove" help:"manually remove bad address from the list"`
+	Check    *CheckCmd            `arg:"subcommand:check" help:"check whether an address is valid or not"`
+	Verbose  bool                 `arg:"-v" help:"emit additional information"`
 }
 
 func check(err error, context string, formatters ...interface{}) {
@@ -37,7 +46,7 @@ func main() {
 	parser := arg.MustParse(&args)
 
 	// then get the AWS session
-	sess, err := session.NewSession()
+	sess, err := session.NewSession(&aws.Config{Region: aws.String(baddress.Region)})
 	check(err, "creating AWS session")
 	ddb := dynamodb.New(sess)
 
@@ -46,9 +55,11 @@ func main() {
 	case args.Generate != nil:
 		fmt.Println("generate: unimplemented")
 	case args.Add != nil:
-		check(baddress.Add(ddb, *args.Add), "manually adding address")
+		check(baddress.Add(ddb, *args.Add, args.Verbose), "manually adding address")
 	case args.Remove != nil:
-		check(baddress.Remove(ddb, *args.Remove), "manually removing address")
+		check(baddress.Remove(ddb, *args.Remove, args.Verbose), "manually removing address")
+	case args.Check != nil:
+		fmt.Println("check: unimplemented")
 	default:
 		parser.WriteHelp(os.Stdout)
 	}
