1a0d71b5583cccd75c2140907b9c1f6150debf5b
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2019-09-13 13:27:10 +0200

    include bash in ndauimage; our scripts require it

--- docker/bin/exploreimage.sh
+++ docker/bin/exploreimage.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+image="$1"
+if [ -z "$image" ]; then
+    image=ndauimage:latest
+fi
+
 # This starts a shell inside the ndau image.
-docker run --rm -it ndauimage /bin/bash
+docker run --rm -it "$image" /bin/sh
