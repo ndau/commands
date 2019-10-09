94cfe3af8187ff69b25579f025d7f22ee2ab32ee
Kent Quirk <kentquirk@gmail.com>
2019-10-09 13:05:12 -0400

    Add copyright to python files

--- automation/hud.py
+++ automation/hud.py
@@ -1,5 +1,14 @@
 #!/usr/bin/env python3
 
+#  ----- ---- --- -- -
+#  Copyright 2019 Oneiro NA, Inc. All Rights Reserved.
+# 
+#  Licensed under the Apache License 2.0 (the "License").  You may not use
+#  this file except in compliance with the License.  You can obtain a copy
+#  in the file LICENSE in the source distribution or at
+#  https://www.apache.org/licenses/LICENSE-2.0.txt
+#  - -- --- ---- -----
+
 from get_catchup import get_catchup
 from get_health import get_health
 from get_height import get_height
