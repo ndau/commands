37128038b0b5b9cdd8bf88982117a4b1cedd1d7e
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2019-10-29 16:19:29 +0100

    add more primitives to sysvar tool, enable multiple entries of each

--- cmd/sysvar/main.go
+++ cmd/sysvar/main.go
@@ -6,16 +6,24 @@ import (
 	"os"
 
 	"github.com/alexflint/go-arg"
+	"github.com/oneiro-ndev/msgp-well-known-types/wkt"
 	"github.com/oneiro-ndev/ndaumath/pkg/address"
+	math "github.com/oneiro-ndev/ndaumath/pkg/types"
 )
 
 type args struct {
-	Address address.Address `arg:"-a" help:"encode this ndau address"`
+	Address  []address.Address `arg:"-a,separate" help:"encode this ndau address"`
+	Duration []math.Duration   `arg:"-d,separate" help:"encode this duration"`
+	Int64    []int64           `arg:"-i,separate" help:"encode this signed integer"`
+	Ndau     []math.Ndau       `arg:"-n,separate" help:"encode this qty of ndau"`
+	Uint64   []uint64          `arg:"-u,separate" help:"encode this unsigned integer"`
 }
 
 func (args) Description() string {
 	return `
 Encode specific types into appropriate formats for a SetSysvar tx
+
+Each flag may be set multiple times.
 	`
 }
 
@@ -31,14 +39,37 @@ func check(err error, context string, formatters ...interface{}) {
 	}
 }
 
+func output(obj interface{}, bytes []byte) {
+	fmt.Printf("%-50v %s\n", obj, base64.StdEncoding.EncodeToString(bytes))
+}
+
 func main() {
 	var args args
 	arg.MustParse(&args)
 
-	ea := address.Address{}
-	if args.Address != ea {
-		bytes, err := args.Address.MarshalMsg(nil)
-		check(err, "msgp marshaling")
-		fmt.Println(base64.StdEncoding.EncodeToString(bytes))
+	for _, v := range args.Address {
+		bytes, err := v.MarshalMsg(nil)
+		check(err, "msgp marshaling address")
+		output(v.String(), bytes)
+	}
+	for _, v := range args.Duration {
+		bytes, err := v.MarshalMsg(nil)
+		check(err, "msgp marshaling duration")
+		output(v, bytes)
+	}
+	for _, v := range args.Int64 {
+		bytes, err := wkt.Int64(v).MarshalMsg(nil)
+		check(err, "msgp marshaling int64")
+		output(v, bytes)
+	}
+	for _, v := range args.Ndau {
+		bytes, err := v.MarshalMsg(nil)
+		check(err, "msgp marshaling ndau")
+		output(v, bytes)
+	}
+	for _, v := range args.Uint64 {
+		bytes, err := wkt.Uint64(v).MarshalMsg(nil)
+		check(err, "msgp marshaling uint64")
+		output(v, bytes)
 	}
 }
