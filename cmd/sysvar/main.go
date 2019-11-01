bb7d7d9d55f14fd4604411279d9219f1b4d3e209
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2019-11-01 14:32:20 +0100

    handle both binary and string types

--- cmd/sysvar/main.go
+++ cmd/sysvar/main.go
@@ -12,12 +12,13 @@ import (
 )
 
 type args struct {
-	Address   []address.Address `arg:"-a,separate" help:"encode this ndau address"`
-	Duration  []math.Duration   `arg:"-d,separate" help:"encode this duration"`
-	Int64     []int64           `arg:"-i,separate" help:"encode this signed integer"`
-	Ndau      []math.Ndau       `arg:"-n,separate" help:"encode this qty of ndau"`
-	Uint64    []uint64          `arg:"-u,separate" help:"encode this unsigned integer"`
-	StringArg []string          `arg:"-s,separate" help:"encode this stringr"`
+	Address  []address.Address `arg:"-a,separate" help:"encode this ndau address"`
+	Bytes    []string          `arg:"-b,separate" help:"encode these base64'd bytes (i.e. chaincode)"`
+	Duration []math.Duration   `arg:"-d,separate" help:"encode this duration"`
+	Int64    []int64           `arg:"-i,separate" help:"encode this signed integer"`
+	Ndau     []math.Ndau       `arg:"-n,separate" help:"encode this qty of ndau"`
+	String   []string          `arg:"-s,separate" help:"encode this stringr"`
+	Uint64   []uint64          `arg:"-u,separate" help:"encode this unsigned integer"`
 }
 
 func (args) Description() string {
@@ -53,6 +54,13 @@ func main() {
 		check(err, "msgp marshaling address")
 		output(v.String(), bytes)
 	}
+	for _, v := range args.Bytes {
+		bytes, err := base64.StdEncoding.DecodeString(v)
+		check(err, "decoding base64 encoding")
+		bytes, err = wkt.Bytes(bytes).MarshalMsg(nil)
+		check(err, "msgp marshaling bytes")
+		output(v, bytes)
+	}
 	for _, v := range args.Duration {
 		bytes, err := v.MarshalMsg(nil)
 		check(err, "msgp marshaling duration")
@@ -68,14 +76,14 @@ func main() {
 		check(err, "msgp marshaling ndau")
 		output(v, bytes)
 	}
+	for _, v := range args.String {
+		bytes, err := wkt.String(v).MarshalMsg(nil)
+		check(err, "msgp marshaling string")
+		output(v, bytes)
+	}
 	for _, v := range args.Uint64 {
 		bytes, err := wkt.Uint64(v).MarshalMsg(nil)
 		check(err, "msgp marshaling uint64")
-		output(v, bytes)
-	}
-	for _, v := range args.StringArg {
-		bytes, err := wkt.String(v).MarshalMsg(nil)
-		check(err, "msgp marshaling string")
 		output(v, bytes)
 	}
 }
