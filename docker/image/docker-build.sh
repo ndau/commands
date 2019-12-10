b55d6bd1d9ab5b83dda737b442ae1798ae4c72cb
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2019-12-10 14:50:59 +0100

    only test the commands repo

--- docker/image/docker-build.sh
+++ docker/image/docker-build.sh
@@ -34,10 +34,21 @@ mv procmon "$BIN_DIR"
 if [ -n "$RUN_UNIT_TESTS" ]; then
     echo "Running unit tests..."
     export CGO_ENABLED=0
-    for dir in $(go list -f '{{.Dir}}' -m github.com/oneiro-ndev/...)
-    do
-        cd "$dir"
-        pwd
-        go test ./...
-    done
+    go test ./...
+    # unfortunately, we no longer have the ability to test all oneiro
+    # dependencies in any straightforward way.
+    #   go test all
+    # seems like a plausible approach, but it really tests _everything_,
+    # and doesn't offer any subfilters. We don't want to waste time testing,
+    # for example, the whole postgres suite.
+    # we also can't just iterate through the output of
+    #   go list -f '{{.Dir}}' -m github.com/oneiro-ndev/...
+    # and run
+    #   go test ./...
+    # in each produced directory, because those dependencies can _only_ be
+    # dependencies; they don't show up as packages which can be compiled
+    # independently.
+    #
+    # The real solution at this point is to have each of our dependencies
+    # test itself using its own circle job. That's a task for the future.
 fi
