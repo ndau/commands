458c77276850acee3892b4683b016a0d24ebac15
John Greenland <jsgjnr@yahoo.com>
2021-01-20 09:44:28 -0500

    fix NNR catchup failure at block 88542, update nomscompare with better diagnostics, add snapshot arg to catchup script

--- docker/bin/test_catchup.sh
+++ docker/bin/test_catchup.sh
@@ -7,7 +7,11 @@ cd "$SCRIPT_DIR" || exit 1
 
 # Run a local node connected to mainnet starting from the genesis snapshot.
 nodename="catchup-node-local"
-snapshot="snapshot-mainnet-1"
+snapshot="$1"
+if [ -z "$snapshot" ]; then
+    snapshot="snapshot-mainnet-1"
+fi
+
 USE_LOCAL_IMAGE=1 \
             AWS_ACCESS_KEY_ID="" \
             AWS_SECRET_ACCESS_KEY="" \
