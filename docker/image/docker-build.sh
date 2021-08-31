b3b5cc34b8d96567cf091042ae1b6288e8c2c3c1
Ed McNierney <ed@mcnierney.com>
2021-08-31 11:52:43 -0400

    Debug missing claimer conf

--- docker/image/docker-build.sh
+++ docker/image/docker-build.sh
@@ -28,7 +28,7 @@ mv generate "$BIN_DIR"
 mv keytool "$BIN_DIR"
 mv ndau "$BIN_DIR"
 mv claimer "$BIN_DIR"
-mv cmd/claimer/claimer_conf.toml "$BIN_DIR"
+mv cmd/claimer/claimer* "$BIN_DIR"
 
 echo Building procmon...
 go build ./cmd/procmon
