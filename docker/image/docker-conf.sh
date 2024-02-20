aaa12213973faf64d31cb0f07d5edf1318563ef6
Ed McNierney <ed@mcnierney.com>
2024-02-20 11:11:53 -0500

    Update tm timeouts, use reverted Burn tx

--- docker/image/docker-conf.sh
+++ docker/image/docker-conf.sh
@@ -203,11 +203,11 @@ sed -i -E \
     -e 's/^(allow_duplicate_ip =) (.*)/\1 true/' \
     -e 's/^(log_format =) (.*)/\1 "json"/' \
     -e 's/^(log_level =) (.*)/\1 "'"$TM_LOG_LEVEL"'"/' \
-    -e 's/^(timeout_propose =) (.*)/\1 "60s"/' \
-    -e 's/^(timeout_prevote =) (.*)/\1 "10s"/' \
-    -e 's/^(timeout_precommit =) (.*)/\1 "10s"/' \
-    -e 's/^(timeout_commit =) (.*)/\1 "10s"/' \
-    -e 's/^(timeout_broadcast_tx_commit =) (.*)/\1 "60s"/' \
+    -e 's/^(timeout_propose =) (.*)/\1 "5s"/' \
+    -e 's/^(timeout_prevote =) (.*)/\1 "5s"/' \
+    -e 's/^(timeout_precommit =) (.*)/\1 "5s"/' \
+    -e 's/^(timeout_commit =) (.*)/\1 "5s"/' \
+    -e 's/^(timeout_broadcast_tx_commit =) (.*)/\1 "90s"/' \
     -e 's/^(moniker =) (.*)/\1 "'"$NODE_ID"'"/' \
     "$TM_DATA_DIR/config/config.toml"
 
